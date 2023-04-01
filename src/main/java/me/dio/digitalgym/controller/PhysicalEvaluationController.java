c3e99934aec401441370cd1f7b764169f2e6b8dd
Lucas Feitoza <lucas_feitoza@outlook.com>
2023-04-01 12:52:31 -0300

    feat: created controllers pt1

---
+++ src/main/java/me/dio/digitalgym/controller/PhysicalEvaluationController.java
@@ -0,0 +1,9 @@
+package me.dio.digitalgym.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/evaluations")
+public class PhysicalEvaluationController {
+}
---
+++ src/main/java/me/dio/digitalgym/controller/RegistrationController.java
@@ -0,0 +1,9 @@
+package me.dio.digitalgym.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/registrations")
+public class RegistrationController {
+}
