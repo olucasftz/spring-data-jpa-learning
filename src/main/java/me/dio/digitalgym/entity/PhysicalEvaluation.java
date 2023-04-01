ad96adefef180f4e2b940932dbc9c27d8eeb4660
Lucas Feitoza <lucas_feitoza@outlook.com>
2023-04-01 12:12:07 -0300

    feat: lombok annotations, java persistence and others

--- src/main/java/me/dio/digitalgym/entity/PhysicalEvaluation.java
+++ src/main/java/me/dio/digitalgym/entity/PhysicalEvaluation.java
@@ -1,14 +1,32 @@
 package me.dio.digitalgym.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_evaluations")
 public class PhysicalEvaluation {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "student_id")
     private Student student;
     private LocalDate evaluationDate = LocalDate.now();
 
+    @Column(name = "weight_current")
     private double weight;
+
+    @Column(name = "height_current")
     private double height;
 
 }
--- src/main/java/me/dio/digitalgym/entity/Registration.java
+++ src/main/java/me/dio/digitalgym/entity/Registration.java
@@ -1,10 +1,25 @@
 package me.dio.digitalgym.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_registrations")
 public class Registration {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "student_id")
     private Student student;
     private LocalDate registrationDate;
 }
